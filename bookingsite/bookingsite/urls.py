982449426a439c5da4f9a19fef952f3bef05434d
vladislav erofeev <vladislav.erofeev@zyfra.com>
2023-01-08 13:32:26 +0300

    finished site

--- bookingsite/bookingsite/urls.py
+++ bookingsite/bookingsite/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apartments.urls')),
-    # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
 ]
